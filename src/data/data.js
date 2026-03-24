f96660e6fdd6d19236a65aa18a2d5f826bf3b72e
Joseph Abdullaahi <josephabdullaahi@gmail.com>
2026-03-24 09:30:00 +0300

    feat(data): add product data and utility helpers

---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+export const product = {
+  id: "fall-limited-sneakers",
+  company: "SNEAKER COMPANY",
+  title: "Fall Limited Edition Sneakers",
+  description:
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+  discountPrice: 125.0,
+  originalPrice: 250.0,
+  discountPercentage: 50,
+  thumbnail: "/src/assets/images/image-product-1-thumbnail.jpg",
+};
+
+export const productImages = [
+  "/src/assets/images/image-product-1.jpg",
+  "/src/assets/images/image-product-2.jpg",
+  "/src/assets/images/image-product-3.jpg",
+  "/src/assets/images/image-product-4.jpg",
+];
+
+export const thumbnails = [
+  "/src/assets/images/image-product-1-thumbnail.jpg",
+  "/src/assets/images/image-product-2-thumbnail.jpg",
+  "/src/assets/images/image-product-3-thumbnail.jpg",
+  "/src/assets/images/image-product-4-thumbnail.jpg",
+];
